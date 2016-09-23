bab5c31aebaf6a4276b88c1b6d3fd7456df477bc
lelbarton <lauraelbarton@gmail.com>
2016-09-22 21:39:22 -0700

    Remove accidentally committed code

--- CSftp.java
+++ CSftp.java
@@ -1,9 +1,4 @@
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.PrintWriter;
-import java.net.Socket;
-import java.net.UnknownHostException;
 
 //
 // This is an implementation of a simplified version of a command 
@@ -29,38 +24,6 @@ public class CSftp
             System.out.print("Usage: cmd ServerAddress ServerPort\n");
             return;
         }
-
-        // borrowed from
-        // http://docs.oracle.com/javase/tutorial/networking/sockets/examples/EchoClient.java
-        String hostName = args[0];
-        int portNumber = Integer.parseInt(args[1]);
-
-        try (
-                Socket echoSocket = new Socket(hostName, portNumber);
-                PrintWriter out =
-                        new PrintWriter(echoSocket.getOutputStream(), true);
-                BufferedReader in =
-                        new BufferedReader(
-                                new InputStreamReader(echoSocket.getInputStream()));
-                BufferedReader stdIn =
-                        new BufferedReader(
-                                new InputStreamReader(System.in))
-        ) {
-            String userInput;
-            while ((userInput = stdIn.readLine()) != null) {
-                out.println(userInput);
-                System.out.println("echo: " + in.readLine());
-            }
-        } catch (UnknownHostException e) {
-            System.err.println("Don't know about host " + hostName);
-            System.exit(1);
-        } catch (IOException e) {
-            System.err.println("Couldn't get I/O for the connection to " +
-                    hostName);
-            System.exit(1);
-        }
-
-        // end borrowed code
 
         try {
             for (int len = 1; len > 0;) {
